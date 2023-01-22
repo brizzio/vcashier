d894c219d6f5ead2c4cdb9fbcc89d26687f54749
brizzio <fabrizio.salvade@gmail.com>
2023-01-22 14:49:27 -0300

    navigation test

--- src/pages/Cashier.jsx
+++ src/pages/Cashier.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Cashier() {
   return (
+    <>
     <div>Cashier</div>
+    <Link to="/login">go to Login Page</Link>
+    </>
+    
   )
 }
 
