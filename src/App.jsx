50e27a736d51c21a767c0dbac9f207bc80ff919e
brizzio <fabrizio.salvade@gmail.com>
2023-01-22 14:31:08 -0300

    router changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import React from "react"
+import Login from "./pages/Login"
+import Cashier from "./pages/Cashier"
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 
 function App() {
  
 
   return (
-   <div className="w-screen h-screen bg-red-300">
+   <div className="flex justify-center items-center w-screen h-screen bg-red-300">
+
+    <div className="w-5/6 h-5/6 bg-white">
+    <Router>
+      <Routes>
+        <Route index element={<Cashier/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes> 
+    </Router>
+        
+
+    </div>
 
    </div>
   )
---
+++ src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import React , { useContext, useEffect } from 'react'
+
+export default function Login() {
+
+ 
+  
+
+  
+  
+
+  const handleLogin = (type) => {
+    //Prevent page reload
+    
+   console.log('Login as ', type )
+       
+  };
+
+ 
+
+  return (
+
+    <>
+      <article className='grid h-screen place-items-center '>
+          <div className='flex flex-col gap-4 p-6 w-2/6 h-2/6 items-center justify-center border-gray border-2 shadow-md rounded-lg'>
+
+            <h1>CASSA MAREL</h1>
+            
+            <button variant="main" size="small" className="min-w-9 h-12" onClick={() => handleLogin('admin')}>Accessa come Amministratore</button>
+            
+
+           
+            <button variant="main" size="small" className="min-w-9 h-12 " onClick={() => handleLogin('cassa')}>Accessa come Operatore Cassa</button>
+            
+
+
+          </div>
+          
+
+         
+        
+
+
+
+      </article>
+    
+    </>
+    
+  )
+  
+}
